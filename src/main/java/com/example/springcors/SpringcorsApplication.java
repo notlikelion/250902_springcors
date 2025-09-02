83d26fcf47c01eecbeb53d864f3546ec113e65ba
notlikelion <notlikelion@gmail.com>
2025-09-02 15:38:53 +0900

    Initial commit

---
+++ src/main/java/com/example/springcors/SpringcorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcors/SpringcorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
