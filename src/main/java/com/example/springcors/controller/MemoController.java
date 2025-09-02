76d9375feacf3804212cbdf7605606a2a9d1193b
notlikelion <notlikelion@gmail.com>
2025-09-02 16:09:03 +0900

    푸시푸시~

--- src/main/java/com/example/springcors/controller/MemoController.java
+++ src/main/java/com/example/springcors/controller/MemoController.java
@@ -1,14 +1,36 @@
 package com.example.springcors.controller;
 
 import com.example.springcors.domain.Memo;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@RequestMapping("/api/memos")
 public class MemoController {
 //    private final CopyOnWriteArrayList<Memo> memos = new CopyOnWriteArrayList<>();
     private final ConcurrentHashMap<Long, Memo> memos = new ConcurrentHashMap<>();
     private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping
+    public List<Memo> getMemos() {
+        return memos.values().stream().toList();
+    }
+
+    @PostMapping
+    public Memo createMemo(@RequestBody Memo.MemoDTO dto) {
+        long newId = counter.incrementAndGet();
+        Memo memo = new Memo(newId, dto.content());
+        memos.put(newId, memo);
+        return memo;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMemo(@PathVariable Long id) {
+        memos.remove(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/example/springcors/domain/Memo.java
+++ src/main/java/com/example/springcors/domain/Memo.java
@@ -1,4 +1,5 @@
 package com.example.springcors.domain;
 
 public record Memo(Long id, String content) {
+    public record MemoDTO(String content) {}
 }
